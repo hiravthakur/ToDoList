d5d5be2863109f975ce9bac341268f78997e67a5
Hirav T. <hirav.a.s.thakur@gmail.com>
2025-04-15 18:42:37 -0400

    set up class and struct definitions for toDoList

---
+++ include/ToDoList.hpp
@@ -0,0 +1,35 @@
+#ifndef TODOLIST_HPP
+
+#define TODOLIST_HPP
+
+#include <vector>
+#include <string>
+
+//ToDoItem in a list
+struct item {
+    int priority;
+    std::string name;
+};
+
+
+//List that holds items + class methods
+class ToDoList {
+private:
+    std::vector<item> lstItems; 
+public:
+
+    //adds item to the toDoList based on Priority
+    bool addItem(int priority, std::string& desc);
+
+    //Sorts the items in the list based on prioritty
+    std::vector<item> sortItems();
+
+    //Delete an item based on their current position
+    bool deleteItemByIndex(int i);
+
+    //return an array of integers missing in priority
+    std::vector<int> missingPriorities();
+
+};
+
+#endif
