422a14667e5c1e9bb58cf27a5582d29ad4a35da9
Hirav T. <hirav.a.s.thakur@gmail.com>
2025-04-16 01:59:50 -0400

    got the functions working, basic functionality ready.

--- src/ToDoList.cpp
+++ src/ToDoList.cpp
@@ -2,3 +2,59 @@
 #include <algorithm>
 #include <set>
 
+using namespace std;
+
+void ToDoList::addItem(int priority, std::string& desc) {
+    
+    lstItems.push_back({priority, desc});
+    return;
+}
+
+vector<item> ToDoList::sortItems() {
+    vector<item> sorted = lstItems;
+
+    sort(sorted.begin(), sorted.end(), [](const item &a, const item &b){return a.priority < b.priority;});
+
+    return sorted;
+}
+
+bool ToDoList::deleteItemByIndex(int i) {
+    vector<item> sortedLst = sortItems();
+    int len = sortedLst.size();
+    if (i < 0 or i >= len) {
+        return false;
+    }
+    else {
+        item toDel = sortedLst[i];
+
+        for (auto it = lstItems.begin(); it != lstItems.end(); ++it) {
+            if (it->name == toDel.name && it->priority == toDel.priority){
+                lstItems.erase(it);
+                return true;
+            }
+        }
+    }
+    return false;
+
+}
+
+std::vector<int> ToDoList::missingPriorities() {
+    vector<item> sorted = sortItems();
+    vector<int> missingPriority;
+    if (sorted.empty()) {
+        return missingPriority;
+    }
+    set<int> uniqueVals;
+    for (const auto& i : sorted) {
+        uniqueVals.insert(i.priority);
+    }
+
+    int maxPriority = sorted.back().priority;
+
+    for (int i = 1; i < maxPriority; ++i) {
+        if (uniqueVals.find(i) == uniqueVals.end()) {
+            missingPriority.push_back(i);
+        }
+    }
+    return missingPriority;
+}
--- ui/cli.cpp
+++ ui/cli.cpp
@@ -39,35 +39,76 @@ void inputHandler(ToDoList& inLst) {
             }
             cout << "Enter a task description:\n";
             getline(cin, holdLine);
-            //add create task here
+            inLst.addItem(priority, holdLine);
             cout << "Task created successfully.\n";
             cout << "*************SUCCESS********************\n";
             continue;
         } else if (inChar == 'd') {
+            vector<item> sorted = inLst.sortItems();
+            int len = sorted.size();
+            if (sorted.empty()) {
+                cout << "No items in list.\n";
+                cout << "*************ERROR********************\n";
+                continue;
+            }
+            cout << "Current items in list:\n";
+            for (int i = 0; i < len; ++i) {
+                cout << "-------------\n";
+                cout << "Priority: " << sorted[i].priority << " \n";
+                cout << "Description: " << sorted[i].name << " " " \n";
+                cout << "-------------\n";
+            }
             int index;
             cout << "Enter the index of the task you wish to delete:\n";
             cin >> index;
             cin.ignore();
-            //make a test for index being out of bounds
-
-            //test to see if delete is done successfully
+            if (inLst.deleteItemByIndex(index)) {
             cout << "Task deleted successfully.\n";
             cout << "*************SUCCESS********************\n";
             continue;
+            } else {
+                cout << "Task delete failed.\n";
+            cout << "*************ERROR********************\n";
+            continue;
+            }
         } else if (inChar == 'l') {
-            // check if list is empty
-
+            vector<item> sorted = inLst.sortItems();
+            int len = sorted.size();
+            if (sorted.empty()) {
+                cout << "No Tasks to List.\n";
+                continue;
+            }else {
+                cout << "Sorted items in list:\n";
+                for (int i = 0; i < len; ++i) {
+                    cout << "-------------\n";
+                    cout << "Priority: " << sorted[i].priority << " \n";
+                    cout << "Description: " << sorted[i].name << " " " \n";
+                    cout << "-------------\n";
+                }
             cout << "List sorted by priorities successfully.\n";
             cout << "*************SUCCESS********************\n";
             continue;
+            }
         } else if (inChar == 'm') {
-            //check if list is empty
-
-            //cout the missing priorities
+            vector<int> missing = inLst.missingPriorities();
+            if (missing.empty()) {
+                cout << "There are no missing priorities.\n";
+                continue;
+            } else{
+                int len = missing.size();
+                cout << "Missing Priorities:\n";
+                for (int i = 0; i < len; ++i) {
+                    cout << missing[i];
+                    if (i != len - 1) {
+                    cout << ", ";
+                    }
+                }
+                cout << "\n";
             cout << "*************SUCCESS********************\n";
-            continue;
+            continue; }
         } else if (inChar == 'x') {
             cout << "Exit request accepted.\n";
+            cout << "Created by Hirav Thakur || github/hiravthakur ||\n";
             break;
         }
         else {
